95a2767bf03f4b0317b070b90509d496b3dc9268
zzheadmin <84074950+zzheadmin@users.noreply.github.com>
2021-07-13 14:06:19 +0800

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println( "Hello World!");
         System.out.println("zzz不能这样堕落下去了呀");
+        System.out.println("github项目更新");
     }
 }
